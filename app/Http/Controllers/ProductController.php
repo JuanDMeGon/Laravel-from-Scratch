803342a4cd62daf865873bb23f7c102ec13f303e
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-22 17:30:06 -0500

    The Middleware and How to Restrict Access and Actions in Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,16 @@ use App\Product;
 
 class ProductController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $products = Product::all();
