cb43d40ba25507ad906040b72f02146e24bcda9e
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-23 14:47:50 -0500

    Taking Advantage of Layouts and Components with Laravel Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome</h1>
-    <p>Let's start!</p>
+    @empty ($products)
+        <div class="alert alert-warning">
+            The list of products is empty
+        </div>
+    @else
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-3">
+                    @include('components.product-card')
+                </div>
+            @endforeach
+        </div>
+    @endif
 @endsection
