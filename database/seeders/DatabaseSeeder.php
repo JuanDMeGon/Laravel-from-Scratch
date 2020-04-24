f7ad05c7169b1136b2ae03d5bda285eaaf06b545
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-24 17:20:45 -0500

    Using and Inserting Polymorphic Relationships Using the Laravel Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Cart;
+use App\Image;
 use App\Order;
 use App\Payment;
 use App\Product;
@@ -17,9 +19,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $products = Product::factory(50)->create();
+        $users = User::factory(20)
+            ->create()
+            ->each(function ($user) {
+                $image = Image::factory()
+                    ->user()
+                    ->make();
 
-        $users = User::factory(20)->create();
+                $user->image()->save($image);
+            });
 
         $orders = Order::factory(10)
             ->make()
@@ -33,6 +41,27 @@ class DatabaseSeeder extends Seeder
                 // $payment->save();
 
                  $order->payment()->save($payment);
+            });
+
+        $carts = Cart::factory(20)->create();
+
+        $products = Product::factory(50)
+            ->create()
+            ->each(function ($product) use($orders, $carts) {
+                $order = $orders->random();
+
+                $order->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 3)]
+                ]);
+
+                $cart = $carts->random();
+
+                $cart->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 3)]
+                ]);
+
+                $images = Image::factory(mt_rand(2, 4))->make();
+                $product->images()->saveMany($images);
             });
     }
 }
