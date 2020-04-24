55e3f8e447e7c05e0ef927d19992828a8ed65d5c
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-24 17:47:08 -0500

    Meet and Use Eloquent Scopes to Improve Products Visualization

--- app/Product.php
+++ app/Product.php
@@ -39,4 +39,9 @@ class Product extends Model
     {
         return $this->morphMany(Image::class, 'imageable');
     }
+
+    public function scopeAvailable($query)
+    {
+        return $query->where('status', 'available');
+    }
 }
