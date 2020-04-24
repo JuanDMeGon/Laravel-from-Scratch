abe673982b7c29727e11af005fca1dae49b8eb6d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-24 18:49:31 -0500

    Use Dependency Injection in Laravel to Display the Products of a Cart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('products.index') }}">Products</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('carts.index') }}">Cart</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
