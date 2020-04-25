e2efdf2cdf905be794e45e95e817ed943375f675
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-25 15:10:58 -0500

    Take Advantage of the Service to Remove the Cart Products with Laravel

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -6,13 +6,25 @@
         <p class="card-text">{{ $product->description }}</p>
         <p class="card-text"><strong>{{ $product->stock }} left</strong></p>
 
-        <form
-            class="d-inline"
-            method="POST"
-            action="{{ route('products.carts.store', ['product' => $product->id]) }}"
-        >
-            @csrf
-            <button class="btn btn-success" type="submit">Add to Cart</button>
-        </form>
+        @if (isset($cart))
+            <form
+                class="d-inline"
+                method="POST"
+                action="{{ route('products.carts.destroy', ['product' => $product->id, 'cart' => $cart->id]) }}"
+            >
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-warning" type="submit">Remove From Cart</button>
+            </form>
+        @else
+            <form
+                class="d-inline"
+                method="POST"
+                action="{{ route('products.carts.store', ['product' => $product->id]) }}"
+            >
+                @csrf
+                <button class="btn btn-success" type="submit">Add to Cart</button>
+            </form>
+        @endif
     </div>
 </div>
