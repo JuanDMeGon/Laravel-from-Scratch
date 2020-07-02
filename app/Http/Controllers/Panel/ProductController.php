113bcafee2028f88b31425dd888fdce2ba87a491
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-07-02 16:22:56 -0500

    Ignoring the Global Scopes in the Admin Panel with Laravel

--- app/Http/Controllers/Panel/ProductController.php
+++ app/Http/Controllers/Panel/ProductController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductRequest;
-use App\Product;
+use App\PanelProduct;
 use App\Scopes\AvailableScope;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::withoutGlobalScope(AvailableScope::class)->get();
+        $products = PanelProduct::all();
 
         return view('products.index')->with([
             'products' => $products,
@@ -25,7 +25,7 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request)
     {
-        $product = Product::create($request->validated());
+        $product = PanelProduct::create($request->validated());
 
         return redirect()
             ->route('products.index')
@@ -33,21 +33,21 @@ class ProductController extends Controller
         // ->with(['success' => "New product with id {$product->id} was created"]);
     }
 
-    public function show(Product $product)
+    public function show(PanelProduct $product)
     {
         return view('products.show')->with([
             'product' => $product,
         ]);
     }
 
-    public function edit(Product $product)
+    public function edit(PanelProduct $product)
     {
         return view('products.edit')->with([
             'product' => $product,
         ]);
     }
 
-    public function update(ProductRequest $request, Product $product)
+    public function update(ProductRequest $request, PanelProduct $product)
     {
         $product->update($request->validated());
 
@@ -56,7 +56,7 @@ class ProductController extends Controller
             ->withSuccess("The product with id {$product->id} was updated");
     }
 
-    public function destroy(Product $product)
+    public function destroy(PanelProduct $product)
     {
         $product->delete();
 
